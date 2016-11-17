386255aba69a44b0a6361cf5e7f7fc6da8667947
changshun <chosen.shih@gmail.com>
2016-11-17 22:34:52 +0800

    remove lambdaValues

--- R/Nelson.Siegel.R
+++ R/Nelson.Siegel.R
@@ -4,20 +4,20 @@ function( rate, maturity )
     rate <- try.xts(rate,error=as.matrix)
     if(ncol(rate)==1) rate<-matrix(as.vector(rate),1,nrow(rate))
     pillars.number <- length(maturity)
-    lambdaValues <- 1/seq(maturity[1], maturity[pillars.number], by=0.5)
-    lambdaValues <- lambdaValues[lambdaValues < 1]
+    #lambdaValues <- 1/seq(maturity[1], maturity[pillars.number], by=0.5)
+    #lambdaValues <- lambdaValues[lambdaValues < 1]
 
     FinalResults <- matrix(0, nrow(rate), 4)
     colnames( FinalResults ) <- c("beta_0","beta_1","beta_2","lambda")
     j <- 1
     while(j <= nrow(rate) )
       {
-        InterResults <- matrix(0, length(lambdaValues), 5)
+        InterResults <- matrix(0, length(maturity), 5)
         colnames( InterResults ) <- c("beta0","beta1","beta2","lambda","SSR")
-        for( i in 1:length(lambdaValues))
+        for( i in 1:length(maturity))
           {
             lambdaTemp <- optimize(.factorBeta2,interval=c(0.001,1),
-              maturity=1/lambdaValues[i],maximum=TRUE)$maximum
+              maturity=maturity[i],maximum=TRUE)$maximum
             InterEstimation <- .NS.estimator(as.numeric(rate[j,]), maturity, lambdaTemp)
             BetaCoef <- InterEstimation$Par
 	    if( BetaCoef[1]>0 & BetaCoef[1]<20)
@@ -26,7 +26,7 @@ function( rate, maturity )
                 InterResults[i,] <- c(BetaCoef, lambdaTemp, SSR)
               } else
             {
-              InterResults[i,] <- c(BetaCoef,lambdaValues[i],1e+5)
+              InterResults[i,] <- c(BetaCoef,1/maturity[i],1e+5)
             } 
           }
         BestRow <- which.min(InterResults[,5])
